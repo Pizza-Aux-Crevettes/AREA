812308ad5457f18ef5cf0f9093c1efbc3da8fb2b
Masse Elyne <elyne.masse@epitech.eu>
2024-09-18 16:13:44 +0200

    ADD: button in the center

---
+++ FrontendWeb/src/dashboard/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from '@mantine/core';
+import './Dashboard.css'
+
+function Dashboard() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '98vh', width: '100%' }}>
+      <Button variant="filled">Violet ?</Button>
+    </div>
+  )
+}
+
+export default Dashboard;
