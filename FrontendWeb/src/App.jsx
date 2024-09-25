142c5e2ec47870a04b3e1cbb83a603e39ae115e5
Masse Elyne <elyne.masse@epitech.eu>
2024-09-25 16:38:50 +0200

    ADD: asset and service connection page web

--- FrontendWeb/src/App.jsx
+++ FrontendWeb/src/App.jsx
@@ -1,3 +1,4 @@
+import { MantineProvider } from '@mantine/core';
 import { useState } from "react";
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
@@ -5,17 +6,23 @@ import "./App.css";
 import Login from "./Login/Login";
 import Signup from "./SignUp/SignUp";
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import '@mantine/core/styles.css';
+import Service from './ServiceConnection/ServiceConnection';
 
 const App = () => {
     return (
         <div>
-            <Router>
+            <MantineProvider withGlobalStyles withNormalizeCSS>
+                <Service/>
+            </MantineProvider>
+
+            {/* <Router>
                 <Routes>
                     <Route path="/" element={<Signup />} />
                     <Route path="/Login" element={<Login />} />
                 </Routes>
             </Router>
-            <></>
+            <></> */}
         </div>
     );
 };
