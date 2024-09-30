92fedf4ea31f9d56a13745b7f88b6cc0025173e4
Anastasia Bouby <anastasia.bouby@epitech.eu>
2024-09-30 15:01:06 +0200

    ADD: Backend for login user

--- Backend/src/routes/users/users.ts
+++ Backend/src/routes/users/users.ts
@@ -1,5 +1,5 @@
 import { Express, Request, Response, Router } from "express";
-import { createUsers } from "./users.query";
+import { createUsers, loginUsers, verifyPwd } from "./users.query";
 import bcrypt from "bcryptjs";
 
 module.exports = (app: Express) => {
@@ -18,6 +18,28 @@ module.exports = (app: Express) => {
         if (result === null) {
             res.status(500).json({
                 msg: "Error when setting new user",
+            });
+            return;
+        }
+        res.status(200).json(result);
+    });
+
+    app.post("/api/getUsers", async (req: Request, res: Response) => {
+        console.log(req.body);
+
+        res.setHeader("Content-Type", "application/json");
+        const user_infos = req.body;
+        const result = await loginUsers(user_infos.email);
+        if (result === null) {
+            res.status(500).json({
+                msg: "Error when getting user",
+            });
+            return;
+        }
+        const samePwd = verifyPwd(user_infos.password, result.password);
+        if (!samePwd) {
+            res.status(500).json({
+                msg: "Error when getting user",
             });
             return;
         }
