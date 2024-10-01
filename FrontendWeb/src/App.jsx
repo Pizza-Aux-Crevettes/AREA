9e6b9a55d07b50d570c34173e1190a722936fbff
Masse Elyne <elyne.masse@epitech.eu>
2024-10-01 14:40:47 +0200

    FIX: routes in app.jsx

--- FrontendWeb/src/App.jsx
+++ FrontendWeb/src/App.jsx
@@ -4,7 +4,6 @@ import "./App.css";
 import Login from "./Login/Login";
 import Register from "./Register/Register";
 import Dashboard from "./Dashboard/Dashboard";
-import Service from "./ServiceConnection/ServiceConnection";
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import "@mantine/core/styles.css";
 import Cookies from "cookies-js";
@@ -15,10 +14,6 @@ const App = () => {
             <MantineProvider withGlobalStyles withNormalizeCSS>
                 <Router>
                     <Routes>
-                        <Route path="/" element={<Register />} />
-                        <Route path="/Login" element={<Login />} />
-                        <Route path="/Dashboard" element={<Dashboard />} />
-                        <Route path="/Service" element={<Service />}/>
                         {Cookies.get("token") ? (
                             <>
                                 <Route path="/" element={<Dashboard />} />
