009d57a9f7a923c913df1cbf27c18f3ee927053d
Masse Elyne <elyne.masse@epitech.eu>
2024-10-02 18:00:41 +0200

    ADD: register page in FrontWebApp

--- FrontendApp/src/app/app-routing.module.ts
+++ FrontendApp/src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule) },
-  // Ajoutez d'autres routes ici
+  { path: 'register',loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)},
 ];
 
 @NgModule({
