1fbb2acff4fc759fc9cc542808b023fae149111a
Anastasia Bouby <anastasia.bouby@epitech.eu>
2024-10-06 22:29:31 +0200

    ADD: new routes in tests

--- Backend/src/__test__/basic.test.ts
+++ Backend/src/__test__/basic.test.ts
@@ -6,18 +6,18 @@ const app = createTestServer();
 describe("Auth", () => {
     describe("Error 500", () => {
         it("Login user with missing informations", async () => {
-            await supertest(app).post("/api/getUsers").expect(500);
+            await supertest(app).post("/api/login").expect(500);
         });
 
         it("Login user with wrong informations", async () => {
             await supertest(app)
-                .post("/api/getUsers")
+                .post("/api/login")
                 .send({ email: "test@gmail.com", password: "123abc" })
                 .expect(500);
         });
 
         it("Login user with missing informations", async () => {
-            await supertest(app).post("/api/setUsers").expect(500);
+            await supertest(app).post("/api/register").expect(500);
         });
     });
 });
