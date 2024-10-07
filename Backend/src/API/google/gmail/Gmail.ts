0bbbef2b6f5420f18ae09000d7ef08846a29c388
Perrine Casbas <perrine.casbas@epitech.eu>
2024-10-07 17:23:07 +0200

    ADD: back for sending an email

--- Backend/src/API/google/gmail/Gmail.ts
+++ Backend/src/API/google/gmail/Gmail.ts
@@ -1,11 +1,24 @@
 import { Express, Request, Response } from 'express';
-import { getGmailMsg } from './Gmail.query';
+import { getGmailMsg, sendGmail } from './Gmail.query';
 
 module.exports = (app: Express) => {
     app.post('/api/gmail/msg', async (req: Request, res: Response) => {
         res.setHeader('Content-Type', 'application/json');
         const info = req.body;
         const result = await getGmailMsg(info.token);
+        if (result === null) {
+            res.status(500).json({
+                msg: 'Error when fetching user mail',
+            });
+            return;
+        }
+        res.status(200).json(result.snippet);
+    });
+
+    app.post('/api/gmail/send', async (req: Request, res: Response) => {
+        res.setHeader('Content-Type', 'application/json');
+        const info = req.body;
+        const result = await sendGmail(info.token, info.dest, info.mess);
         if (result === null) {
             res.status(500).json({
                 msg: 'Error when fetching user mail',
