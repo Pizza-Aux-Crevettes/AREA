21df24d1427c60153efc68a640b0c2c01f33bf02
Masse Elyne <elyne.masse@epitech.eu>
2024-10-09 10:46:05 +0200

    FIX: redirect spotify_token

--- Backend/src/API/Spotify.ts
+++ Backend/src/API/Spotify.ts
@@ -43,11 +43,11 @@ module.exports = (app: Express) => {
             const access_token = response.data.access_token;
             const refresh_token = response.data.refresh_token;
 
-            let redirectUrl;
-            const origin = req.headers.referer;
-            redirectUrl = `${origin}service?spotify_token=${access_token}`;
+            
+            res.redirect(
+                `http://localhost:8081/service?spotify_token=${access_token}`
+            );
 
-            res.redirect(redirectUrl);
         } catch (error) {
             console.error('Error retrieving access token:', error);
             res.send('Error during token retrieval');
