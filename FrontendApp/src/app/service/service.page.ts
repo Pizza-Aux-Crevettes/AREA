54a778eb5c0dbea0fb61879f0d3e748f3b4a28f5
Anastasia Bouby <anastasia.bouby@epitech.eu>
2024-10-13 18:52:03 +0200

    ADD: connection to services && stock of token in local storage

--- FrontendApp/src/app/service/service.page.ts
+++ FrontendApp/src/app/service/service.page.ts
@@ -1,21 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-import { TokenService } from 'src/app/services/token/token.service';
-import { SpotifyService } from 'src/app/services/spotify/spotify.service';
-import { RegisterService } from 'src/app/services/register/register.service';
-import { WeatherService } from 'src/app/services/weather/weather.service';
 import { LocalStorageService } from '../services/localStorage/localStorage.service';
 import { Router } from '@angular/router';
+import { environment } from '../../environments/environment';
 
 @Component({
     selector: 'app-service',
     templateUrl: './service.page.html',
     styleUrls: ['./service.page.scss'],
 })
-export class ServicePage {
+export class ServicePage implements OnInit {
+    serviceList: string[] = [
+        'spotify_token',
+        'x_token',
+        'google_token',
+        'discord_token',
+    ];
+
     constructor(
         private localStorage: LocalStorageService,
         private router: Router
     ) {}
+
+    ngOnInit() {
+        const search = window.location.search;
+        const params = new URLSearchParams(search);
+        let token = '';
+        for (let i = 0; i < this.serviceList.length; i++) {
+            if (params.get(this.serviceList[i])) {
+                token = `${params.get(this.serviceList[i])}`;
+                this.localStorage.setItem(this.serviceList[i], token);
+            }
+        }
+    }
 
     deleteCookies() {
         console.log('test');
@@ -29,6 +45,6 @@ export class ServicePage {
     }
 
     connectService(service: string) {
-        window.location.href = 'http://localhost:8080/' + service + '/login';
+        window.location.href = `${environment.api}/${service}/login`;
     }
 }
