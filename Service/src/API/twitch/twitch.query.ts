b53fcf396e774a09a8645fa27c7be5b1f118b4b5
Perrine Casbas <perrine.casbas@epitech.eu>
2024-10-21 22:08:48 +0200

    ADD: service create a twitch clip

---
+++ Service/src/API/twitch/twitch.query.ts
@@ -0,0 +1,54 @@
+import dotenv from 'dotenv';
+import * as base64 from 'base64-url';
+import supabase from '../../config/db';
+import e from 'express';
+
+dotenv.config();
+
+async function getUserId(username: string, token: string) {
+    let response;
+    try {
+        const result = await fetch(
+            `https://api.twitch.tv/helix/users?login=${username}`,
+            {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: 'Bearer ' + token,
+                    'Client-Id': process.env.TWITCH_CLIENT_ID!,
+                },
+            }
+        );
+
+        response = await result.json();
+    } catch (error) {
+        console.error('error', error);
+        return null;
+    }
+    return response.data[0].id;
+}
+
+export async function clipTwitch(username: string, token: string) {
+    const idUsername = await getUserId(username, token);
+    let data;
+    try {
+        const result = await fetch(
+            `https://api.twitch.tv/helix/clips?broadcaster_id=${idUsername}`,
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: 'Bearer ' + token,
+                    'Client-Id': process.env.TWITCH_CLIENT_ID!,
+                },
+            }
+        );
+
+        data = await result.json();
+    } catch (error) {
+        console.error('error', error);
+        return null;
+    }
+    console.log(data)
+    return data;
+}
