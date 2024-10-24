6d7bc29988a853762a5e52665efe9ac51d9ac61a
Anastasia Bouby <anastasia.bouby@epitech.eu>
2024-10-24 18:39:11 +0200

    ADD : block existing field for app

--- FrontendApp/src/app/dashboard/dashboard.page.ts
+++ FrontendApp/src/app/dashboard/dashboard.page.ts
@@ -19,48 +19,47 @@ interface Area {
     styleUrls: ['./dashboard.page.scss'],
     providers: [AreaService],
 })
-
 export class DashboardPage implements OnInit {
     hoverText: string = '';
     @ViewChild('menu', { static: false }) menu!: IonSelect;
 
     areas: Area[] = [];
-    
+
     serviceList: string[] = [
-      'spotify_token',
-      'twitch_token',
-      'google_token',
-      'discord_token',
-      'spotify_refresh',
-      'google_refresh',
-      'twitch_refresh',
-      'discord_refresh',
+        'spotify_token',
+        'twitch_token',
+        'google_token',
+        'discord_token',
+        'spotify_refresh',
+        'google_refresh',
+        'twitch_refresh',
+        'discord_refresh',
     ];
-  
+
     cities: any[] = [
-      { name: 'Paris' },
-      { name: 'Marseille' },
-      { name: 'Lyon' },
-      { name: 'Toulouse' },
-      { name: 'Nice' },
-      { name: 'Nantes' },
-      { name: 'Montpellier' },
-      { name: 'Strasbourg' },
-      { name: 'Bordeaux' },
-      { name: 'Lille' },
+        { name: 'Paris' },
+        { name: 'Marseille' },
+        { name: 'Lyon' },
+        { name: 'Toulouse' },
+        { name: 'Nice' },
+        { name: 'Nantes' },
+        { name: 'Montpellier' },
+        { name: 'Strasbourg' },
+        { name: 'Bordeaux' },
+        { name: 'Lille' },
     ];
-  
+
     cities_alerts = [
-      { name: 'Tokyo' },
-      { name: 'Jakarta' },
-      { name: 'Manille' },
-      { name: 'Port-au-Prince' },
-      { name: 'Mexico City' },
-      { name: 'Los Angeles' },
-      { name: 'Calcutta' },
-      { name: 'Dhaka' },
-      { name: 'Caracas' },
-      { name: 'Christchurch' },
+        { name: 'Tokyo' },
+        { name: 'Jakarta' },
+        { name: 'Manille' },
+        { name: 'Port-au-Prince' },
+        { name: 'Mexico City' },
+        { name: 'Los Angeles' },
+        { name: 'Calcutta' },
+        { name: 'Dhaka' },
+        { name: 'Caracas' },
+        { name: 'Christchurch' },
     ];
 
     menuItemsAction = [
@@ -72,7 +71,11 @@ export class DashboardPage implements OnInit {
             label: 'When my discord username changes',
             connected: false,
         },
-        { action: 'DiscordGuilds', label: 'When my number of discord guilds change', connected: false }
+        {
+            action: 'DiscordGuilds',
+            label: 'When my number of discord guilds change',
+            connected: false,
+        },
     ];
 
     menuItemsReaction = [
@@ -80,15 +83,19 @@ export class DashboardPage implements OnInit {
         { reaction: 'sendEmail', label: 'Send an email', connected: false },
         { reaction: 'MP', label: 'Send a mp', connected: false },
         { reaction: 'Clip', label: 'Create a Twitch clip', connected: false },
-        { reaction: 'Event', label: 'Create a Event on Google Calendar', connected: false },
+        {
+            reaction: 'Event',
+            label: 'Create a Event on Google Calendar',
+            connected: false,
+        },
     ];
 
     constructor(
-      private localStorage: LocalStorageService,
-      private router: Router,
-      private areaService: AreaService
+        private localStorage: LocalStorageService,
+        private router: Router,
+        private areaService: AreaService
     ) {}
-  
+
     ngOnInit() {
         this.areaService
             .getArea(`${this.localStorage.getItem('token')}`)
@@ -101,6 +108,10 @@ export class DashboardPage implements OnInit {
                 }
             });
         this.checkConnection();
+    }
+
+    isAreaDisabled(area: Area): boolean {
+        return area.inputAction !== '';
     }
 
     checkServicesConnexion(area: string): boolean {
@@ -157,110 +168,115 @@ export class DashboardPage implements OnInit {
             );
         }
 
-      const token = this.localStorage.getItem('token');
-      if (token) {
-        this.areaService.getArea(token).subscribe((res) => {
-          this.areas = res;
-          if (this.areas.length === 0) {
-            this.addNewArea();
-            this.addNewArea();
-          }
-        });
-      }
-
-    }
-  
-    deleteCookies() {
-      this.localStorage.removeItem('token');
-      this.serviceList.forEach(service => {
-        if (this.localStorage.getItem(service)) {
-          this.localStorage.removeItem(service);
+        const token = this.localStorage.getItem('token');
+        if (token) {
+            this.areaService.getArea(token).subscribe((res) => {
+                this.areas = res;
+                if (this.areas.length === 0) {
+                    this.addNewArea();
+                    this.addNewArea();
+                }
+            });
         }
-      });
-      this.router.navigate(['/']);
     }
-  
-    moveToPage(navigate: string) {
-      this.router.navigate([navigate]);
-    }
-  
-    addNewArea(): void {
-      const newArea: Area = {
-        id: this.areas.length,
-        action: '',
-        reaction: '',
-        inputAction: '',
-        inputReaction: '',
-        userEmail: '',
-        local: true,
-      };
-      this.areas.push(newArea);
 
+    deleteCookies() {
+        this.localStorage.removeItem('token');
+        this.serviceList.forEach((service) => {
+            if (this.localStorage.getItem(service)) {
+                this.localStorage.removeItem(service);
+            }
+        });
+        this.router.navigate(['/']);
     }
-  
+
+    moveToPage(navigate: string) {
+        this.router.navigate([navigate]);
+    }
+
+    addNewArea(): void {
+        const newArea: Area = {
+            id: this.areas.length,
+            action: '',
+            reaction: '',
+            inputAction: '',
+            inputReaction: '',
+            userEmail: '',
+            local: true,
+        };
+        this.areas.push(newArea);
+    }
+
     onSelectAction(event: any, area: Area) {
-      area.action = event.detail.value;
+        area.action = event.detail.value;
     }
-  
+
     onSelectReaction(event: any, area: Area) {
-      area.reaction = event.detail.value;
+        area.reaction = event.detail.value;
     }
-  
+
     onSelectCity(event: any, area: Area) {
-      area.inputAction = event.detail.value;
+        area.inputAction = event.detail.value;
     }
-  
+
     openMenu(menu: IonSelect) {
         if (menu) {
-          menu.open();
+            menu.open();
         } else {
-          console.log("Menu not found");
+            console.log('Menu not found');
         }
     }
-  
-    onSelectNavigate(event: any) {
-      const selectedValue = event.detail.value;
-      if (selectedValue === 'Dashboard') {
-        this.moveToPage('/dashboard');
-      } else if (selectedValue === 'Service') {
-        this.moveToPage('/service');
-      }
-    }
 
+    onSelectNavigate(event: any) {
+        const selectedValue = event.detail.value;
+        if (selectedValue === 'Dashboard') {
+            this.moveToPage('/dashboard');
+        } else if (selectedValue === 'Service') {
+            this.moveToPage('/service');
+        }
+    }
 
     onSelectParam(event: any) {
         const selectedValue = event.detail.value;
         if (selectedValue === 'Log out') {
-          this.deleteCookies();
+            this.deleteCookies();
         } else if (selectedValue === 'Dislexic font') {
-          this.moveToPage('/service');
+            this.moveToPage('/service');
         }
-      }
+    }
 
     DelArea(id: number) {
-      const token = this.localStorage.getItem('token');
-      if (!token) return;
-  
-      const area = this.areas.find(area => area.id === id);
-      if (area) {
-        const body = {
-          action: area.action,
-          reaction: area.reaction,
-          inputAction: area.inputAction,
-          inputReaction: area.inputReaction,
-        };
-        this.areaService.delArea(token, body).subscribe(() => {
-          this.areas = this.areas.filter(area => area.id !== id);
-        });
-      }
+        const token = this.localStorage.getItem('token');
+        if (!token) return;
+
+        const area = this.areas.find((area) => area.id === id);
+        if (area) {
+            const body = {
+                action: area.action,
+                reaction: area.reaction,
+                inputAction: area.inputAction,
+                inputReaction: area.inputReaction,
+            };
+            this.areaService.delArea(token, body).subscribe(() => {
+                this.areas = this.areas.filter((area) => area.id !== id);
+            });
+        }
     }
-  
-    ApplyArea(action: string, reaction: string, inputAction?: string, inputReaction?: string) {
-      const token = this.localStorage.getItem('token');
-      if (token) {
-        this.areaService.setArea(token, action, reaction, inputAction, inputReaction).subscribe(response => {
-          console.log(response);
-        });
-      }
+
+    ApplyArea(
+        action: string,
+        reaction: string,
+        inputAction?: string,
+        inputReaction?: string
+    ) {
+        const token = this.localStorage.getItem('token');
+        if (token) {
+            this.areaService
+                .setArea(token, action, reaction, inputAction, inputReaction)
+                .subscribe((response) => {
+                    console.log(response);
+                    window.location.reload();
+                });
+        }
     }
-  }
+}
