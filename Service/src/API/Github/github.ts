15f3c37827368dc1b572280b9609d01d3a6d49f1
Perrine Casbas <perrine.casbas@epitech.eu>
2024-10-26 19:25:18 +0200

    DELETE: debug

--- Service/src/API/Github/github.ts
+++ Service/src/API/Github/github.ts
@@ -2,7 +2,6 @@ import { createIssue } from "./github.query";
 
 export async function issueGithub(token:string, infos: string) {
     const info = infos.split(' ');
-    console.log("infos :", info[0], info[1], info[2]);
     const result = createIssue(token, info[0], info[1], info[2])
     if (result !== null) {
         return result;
